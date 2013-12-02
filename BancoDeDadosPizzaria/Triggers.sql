238cb6d5c1ad091bba88144505b38c1255ca7e2d
Matheus Liberato <matheusliberatosbs@gmail.com>
2013-12-02 12:55:29 -0200

    Criação Trigger

--- BancoDeDadosPizzaria/Triggers.sql
+++ BancoDeDadosPizzaria/Triggers.sql
@@ -3,7 +3,8 @@
 *  de do produto no estoque.
 * ******************************************************************************* */
 CREATE TRIGGER tg_Produtos_Pedidos
-ON Produtos_Pedidos, INSERT
+ON Produtos_Pedidos
+AFTER INSERT
 AS
 		BEGIN
 			-- Declara as variaveis a ser utilizada dentro da TRIGGER.
@@ -12,29 +13,58 @@ AS
 			DECLARE @idEstoque INT
 			
 			-- Atribui o código do pedido a variavel.
-			SET @idPedido = (	SELECT Produtos_Pedidos.idPedido 
-												FROM Produtos_Pedidos
-												INNER JOIN inserted
-												ON inserted.idPedido = Produtos_Pedidos.idPedido)
+			SET @idPedido = (SELECT inserted.idPedido FROM inserted)
 
 			-- Atribui a variavel @DataLocacao o valor da data de locacao da tabela locacao.
-			SET @idProduto = (SELECT Produtos_Pedidos.idProduto
-												FROM Produtos_Pedidos
-												INNER JOIN inserted
-												ON inserted.idProduto = Produtos_Pedidos.idProduto)
-
-			SET @idEstoque = (SELECT Estoque.idEstoque
-												FROM Estoque e
-												INNER JOIN Ingredientes i
-												ON e.idEstoque = i.idEstoque
-												INNER JOIN inserted pp
-												ON i.idPedido =  pp.idPedido)
+			SET @idProduto = (SELECT inserted.idProduto FROM inserted)
+												
+			DECLARE @cursorEstoque CURSOR;
+			
+			SET @cursorEstoque = CURSOR FOR
+					SELECT e.idEstoque, e.Quantidade, i.Qtd
+						FROM Estoques e
+						INNER JOIN Ingredientes i
+						ON e.idEstoque = i.idEstoque
+						WHERE i.idProduto = @idProduto;
+						
+			DECLARE @ESTOQUE INT;
+			DECLARE @ESTOQUE_QUANTIDADE INT;
+			DECLARE @INGREDIENTES_QUANTIDADE INT;
+			
+			OPEN @cursorEstoque;
+			
+			IF(CURSOR_STATUS('variable','@cursorEstoque') = 1)
+			BEGIN
+				FETCH NEXT FROM @cursorEstoque INTO
+				@ESTOQUE, @ESTOQUE_QUANTIDADE, @INGREDIENTES_QUANTIDADE;
+				
+				WHILE (@@FETCH_STATUS = 0)
+				BEGIN
+				
+				UPDATE Estoques SET Quantidade = @ESTOQUE_QUANTIDADE - @INGREDIENTES_QUANTIDADE
+				WHERE idEstoque = @ESTOQUE;
+				
+				FETCH NEXT FROM @cursorEstoque INTO
+				@ESTOQUE, @ESTOQUE_QUANTIDADE, @INGREDIENTES_QUANTIDADE;
+				END
+			END
 					
-			PRINT('DEVOLUCAO SUJEITA A MULTA!')
-			PRINT('--------------------')
-			PRINT('Valor da Locação: ')
-			PRINT('Valor da Multa: ') 
-			PRINT('--------------------')
-			PRINT('Total: ') 
+			CLOSE @cursorEstoque;
+			DEALLOCATE @cursorEstoque;
 		END
 GO
+
+/* Select para verificar se a trigger foi executada com sucesso. */
+
+SELECT * FROM Produtos_Pedidos;
+
+INSERT INTO Produtos_Pedidos VALUES (1, 1);
+
+
+SELECT * FROM Estoques e
+INNER JOIN Ingredientes i
+ON e.idEstoque = i.idEstoque
+WHERE i.idProduto = 3
+												
+SELECT * FROM Estoques
+SELECT * FROM Ingredientes
